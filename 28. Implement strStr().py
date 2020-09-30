5ad384992471f8b3474dd327ce9be8bcd301c131
Naman Kanwar <nkanwar1@student.gsu.edu>
2020-09-30 12:08:33 -0400

    Create 28. Implement strStr().py

---
+++ 28. Implement strStr().py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if len(needle) == 0:
+            return 0
+        return haystack.find(needle)
